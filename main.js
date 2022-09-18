4efa5793b481b0044c37ab7c89789bb8f56cfed0
Michael <mikahado@gmail.com>
2022-09-18 19:36:15 -0400

    finishes simple liker lab

--- main.js
+++ main.js
@@ -1,8 +1,71 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+// // Defining text characters for the empty and full hearts for you to use later.
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
+
+
+
+
+fetch(mimicServerCall)
+  .then(res => console.log(res.json))
+
+
+
+
 
 // Your JavaScript code goes here!
+
+// hide error msg
+document.getElementById("modal").hidden = true 
+
+
+
+// //Event Listeners
+// document.querySelector('footer').adEventListeners('click', handleClick)
+
+// //EVent handler
+// function handleClick(e)[
+//   e.preventDefault()
+  
+// ]
+
+
+
+
+// const init = () => {
+  
+
+//               liker.addEventListener('click', event) => {
+//                 event.pretentDefault()
+//               const 
+
+//                 }
+
+//                 document.getElementsByClassName('like-glyph').addEventListener('click', liker)
+
+//                 const configObject = {
+//                   method: "POST",
+//                   headers: {
+//                     "Content-Type": "application/json",
+//                     "Accept": "application/json",
+//                   },
+//                   body: { JSON.stringify({
+                
+//                   })
+                
+//                   }
+//                 };
+                
+//                 .fetch(mimicServerCall, configObject)
+//                 .then(object => object.)
+
+// }
+
+
+
+
+// // //Domload
+// document.addEventListener('DOMLoadedContent', init)
+
 
 
 
